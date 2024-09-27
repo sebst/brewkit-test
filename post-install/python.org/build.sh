8888ff2c48f7c68ad94ef139e8876f2ff8556804
Bas Steins <sebst@users.noreply.github.com>
2024-09-27 22:18:30 +0200

    test post-install

---
+++ post-install/python.org/build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env -S bash --noprofile --norc -o errexit -o pipefail -o noclobber -o nounset -o allexport
+
+
+echo "--- Installing webserver"
+sudo mkdir -p /var/lib/html
+sudo chmod -R a+rw /var/lib/html
+sudo rm /var/lib/html/index.html
+echo "Hello World" > /var/lib/html/index.html
+
+
+echo "--- Adding Start Script"
+cat << 'EOF' > /var/devcontainer.com/python.org.start.sh
+#!/usr/bin/env -S bash --noprofile --norc -o errexit -o pipefail -o noclobber -o nounset -o allexport
+
+python -m http.server -d /var/www/html
+EOF
+
+
+
+echo "--- Adding Service"
+echo "python: /var/devcontainer.com/python.org.start.sh" >> /workspaces/testcontainer/services.proc
+
