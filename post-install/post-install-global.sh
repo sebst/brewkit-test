27bff5a2042d0d0bbc8f544e50ad2d7bc9d7b84f
Bas Steins <sebst@users.noreply.github.com>
2024-12-29 19:55:18 +0100

    Add socket connection

--- post-install/post-install-global.sh
+++ post-install/post-install-global.sh
@@ -2,15 +2,24 @@
 
 echo "--- Running post-installation-global.sh"
 
-
-echo "--- Adding Start Script for dc-ccli"
-cat << 'EOF' > /var/devcontainer.com/dc-ccli.start.sh
+echo "--- Adding Start Script for dc-ccli server"
+cat <<'EOF' >/var/devcontainer.com/dc-ccli.start.sh
 #!/usr/bin/env -S bash --noprofile --norc -o errexit -o pipefail -o noclobber -o nounset -o allexport
 
 dc-ccli server
 EOF
 chmod +x /var/devcontainer.com/dc-ccli.start.sh
 
+echo "--- Adding Start Script for dc-ccli socket"
+cat <<'EOF' >/var/devcontainer.com/dc-ccli.socket.start.sh
+#!/usr/bin/env -S bash --noprofile --norc -o errexit -o pipefail -o noclobber -o nounset -o allexport
 
-echo "--- Adding Service for dc-ccli"
-echo "dcccli: /var/devcontainer.com/dc-ccli.start.sh" >> /var/devcontainer.com/services.proc
+dc-ccli socket
+EOF
+chmod +x /var/devcontainer.com/dc-ccli.socket.start.sh
+
+echo "--- Adding Service for dc-ccli server"
+echo "dcccliserver: /var/devcontainer.com/dc-ccli.start.sh" >>/var/devcontainer.com/services.proc
+
+echo "--- Adding Service for dc-ccli socket"
+echo "dccclisocket: /var/devcontainer.com/dc-ccli.socket.start.sh" >>/var/devcontainer.com/services.proc
